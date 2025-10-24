c679c8ee2bb4ec2803324d4b5223f589ba73ed5e
Daniel Barrow <daniel.barrow@physics.ox.ac.uk>
2025-10-24 12:09:32 +0100

    More explicit enum marking

--- SplineSet/SplineSet_Eigen.cpp
+++ SplineSet/SplineSet_Eigen.cpp
@@ -15,22 +15,22 @@ SplineSet_Eigen::~SplineSet_Eigen() {
 }
 
 void SplineSet_Eigen::SetupMemoryStructure() {
-  Coefficients = Eigen::MatrixXd(GetNSplines(),GetNKnots()*nCoefficientsPerKnot);
+  Coefficients = Eigen::MatrixXd(GetNSplines(),GetNKnots()*SplineCoeff::nCoefficientsPerKnot);
   Weights = Eigen::VectorXd(GetNSplines());
 }
 
 void SplineSet_Eigen::SetKnotCoefficients(int SplineIndex, int KnotIndex, std::vector<double> Coeffs_) {
   for (int iCoeffIndex=0;iCoeffIndex<SplineCoeff::nCoefficientsPerKnot;iCoeffIndex++) {
-    Coefficients(SplineIndex,(KnotIndex*nCoefficientsPerKnot)+iCoeffIndex) = Coeffs_[iCoeffIndex];
+    Coefficients(SplineIndex,(KnotIndex*SplineCoeff::nCoefficientsPerKnot)+iCoeffIndex) = Coeffs_[iCoeffIndex];
   }
 }
 
 void SplineSet_Eigen::EvaluateSplines(double DialValue) {
   double DelX = DialValue-KnotLocations[GetSegmentIndex()];
-  Eigen::VectorXd Y = Coefficients.col(GetSegmentIndex()*nCoefficientsPerKnot+SplineCoeff::CoeffY);
-  Eigen::VectorXd B = Coefficients.col(GetSegmentIndex()*nCoefficientsPerKnot+SplineCoeff::CoeffB);
-  Eigen::VectorXd C = Coefficients.col(GetSegmentIndex()*nCoefficientsPerKnot+SplineCoeff::CoeffC);
-  Eigen::VectorXd D = Coefficients.col(GetSegmentIndex()*nCoefficientsPerKnot+SplineCoeff::CoeffD);
+  Eigen::VectorXd Y = Coefficients.col(GetSegmentIndex()*SplineCoeff::nCoefficientsPerKnot+SplineCoeff::CoeffY);
+  Eigen::VectorXd B = Coefficients.col(GetSegmentIndex()*SplineCoeff::nCoefficientsPerKnot+SplineCoeff::CoeffB);
+  Eigen::VectorXd C = Coefficients.col(GetSegmentIndex()*SplineCoeff::nCoefficientsPerKnot+SplineCoeff::CoeffC);
+  Eigen::VectorXd D = Coefficients.col(GetSegmentIndex()*SplineCoeff::nCoefficientsPerKnot+SplineCoeff::CoeffD);
 
   Weights = Y+DelX*(B+DelX*(C+D*DelX));
 }
